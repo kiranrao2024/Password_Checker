1f65e1c4e287bbc5204701d99e2bf730b85cbeb9
kiranrao2024 <kiran.rao@ufl.edu>
2025-10-10 19:31:50 -0400

    reading txt file into a vector for data structure creation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
+bool readFile(string path, vector<string>& v) {
+    ifstream file(path);
+
+    if (!file) {
+        return false;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        v.push_back(line);
+    }
+
+    return true;
+}
+
 int main() {
-    cout << "DSA Project 2" << endl;
+    vector<string> passwords;
+
+    if (!readFile("../passwords.txt", passwords)) {
+        cerr << "file couldn't be opened" << endl;
+        return 1;
+    }
+
     return 0;
 }
